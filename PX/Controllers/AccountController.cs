4bd5e24ad84df41d77ec4909a15322ef631e4baa
Andersson Hernandez <andersonemanuelhernandezsipaque@yahoo.es>
2018-12-06 14:49:04 -0600

    Cambio con rabbitmq

--- PX/Controllers/AccountController.cs
+++ PX/Controllers/AccountController.cs
@@ -26,6 +26,7 @@ namespace PX.Controllers
         {
             UserManager = userManager;
             SignInManager = signInManager;
+
         }
 
         public ApplicationSignInManager SignInManager
